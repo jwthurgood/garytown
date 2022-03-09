4e0ddee1c88ff47ff64983cea62185fa695a48c9
gwblok <garywblok@gmail.com>
2022-03-08 22:04:11 -0600

    Update UserImages.ps1

--- OSD/CloudOSD/UserImages.ps1
+++ OSD/CloudOSD/UserImages.ps1
@@ -14,11 +14,25 @@ catch{Write-Output "Not in TS"}
 if ($tsenv){$InWinPE = $tsenv.value('_SMSTSInWinPE')}
 
 if ($InWinPE -ne "TRUE"){
+Write-Output "Running in Full OS"
 $InstallPath = "$env:ProgramData\Microsoft\User Account Pictures"
+#Add Required Registry Value
+Write-Output "Adding Value: HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name UseDefaultTile -PropertyType DWORD -Value 1"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "UseDefaultTile" -PropertyType DWORD -Value 1 -Force -Verbose
+
 }
 
 if ($InWinPE -eq "TRUE"){
+Write-Output "Running in WinPE"
+
 $InstallPath = "c:\ProgramData\Microsoft\User Account Pictures"
+#Mount Registry & Add Required Registry Value
+Write-Output "Mounting Offline Registry"
+start-process -FilePath cmd.exe -ArgumentList "/c reg load HKLM\Offline c:\windows\system32\config\software"
+Write-Output "Adding Required Resistry Value"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "UseDefaultTile" -PropertyType DWORD -Value 1 -Force -Verbose
+Write-Output "Dismounting Registry"
+start-process -FilePath cmd.exe -ArgumentList "/c reg unload HKLM\Offline"
 }
 
 $RootURL = "https://raw.githubusercontent.com/gwblok/garytown/master/OSD/CloudOSD/UserPics/"
@@ -41,5 +55,4 @@ foreach ($File in $Files){
     Copy-Item "$env:TEMP\$file" -Destination "$InstallPath\$file" -Force -Verbose
     }
 
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "UseDefaultTile" -PropertyType DWORD -Value 1 -Force -Verbose
-
+Write-Output "Completed updating Default User Images Replacements"
