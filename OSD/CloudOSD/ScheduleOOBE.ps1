40120c89b7443e7fc9ff94c6250081a894e4343d
gwblok <garywblok@gmail.com>
2022-03-26 23:13:43 -0500

    Update ScheduleOOBE.ps1

--- OSD/CloudOSD/ScheduleOOBE.ps1
+++ OSD/CloudOSD/ScheduleOOBE.ps1
@@ -1,5 +1,5 @@
-$action = New-ScheduledTaskAction -Execute "C:\windows\system32\Sysprep\sysprep.exe" -Argument "/OOBE /REBOOT"
+$action = (New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c rd c:\_SMSTaskSequence /S /Q"), (New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c rd c:\MININT /S /Q"),(New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c schtasks.exe/Delete /TN OSDCloudPost /F")
 $trigger = New-ScheduledTaskTrigger -AtStartup
-$settings = New-ScheduledTaskSettingsSet
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings
-Register-ScheduledTask T1 -InputObject $task -User SYSTEM
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries
+$task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Description "OSDCloud Cleanup"
+Register-ScheduledTask OSDCloudPost -InputObject $task -User SYSTEM
