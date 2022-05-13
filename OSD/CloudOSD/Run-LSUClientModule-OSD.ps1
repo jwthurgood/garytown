24a191da09bb0b950400c6a45ff602b903d8b8e0
Jeremy Thurgood <80785999+jwthurgood@users.noreply.github.com>
2022-05-13 11:46:08 -0700

    Update Run-LSUClientModule-OSD.ps1

--- OSD/CloudOSD/Run-LSUClientModule-OSD.ps1
+++ OSD/CloudOSD/Run-LSUClientModule-OSD.ps1
@@ -15,19 +15,7 @@
 .LINK
     https://www.imab.dk/install-lenovo-drivers-and-bios-directly-from-lenovos-driver-catalog-during-osd-using-configuration-manager/
     
-    
-## Set the Installation Policy for PSRepository for PSGallery
-Try { Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-    } Catch {write-output "failed"}
 
-## Install the module LSUClient
-Try { Install-Module -Name LSUClient -Force
-    } Catch {write-output "failed"}
-    
-## Import the module LSUClient
-Try { Import-Module -Name LSUClient -Force
-    } Catch {write-output "failed"}
-    
 #> 
 
 
@@ -60,7 +48,7 @@ Function Write-CMTraceLog {
 		    [int]$Type,
 		
 		    [Parameter(Mandatory=$false)]
-		    $LogFile = "$LogFolder\CloudOSD_LSUClient.log"
+		    $LogFile = $LogFile
 	    )
     <#
     Type: 1 = Normal, 2 = Warning (yellow), 3 = Error (red)
@@ -78,6 +66,7 @@ Function Write-CMTraceLog {
 
 
 function Get-LenovoComputerModel() {
+	Write-CMTraceLog -Message "Running function: Get-LenovoComputerModel" -Type 1
     $lenovoVendor = (Get-CimInstance -ClassName Win32_ComputerSystemProduct).Vendor
     if ($lenovoVendor = "LENOVO") {
         Write-Verbose -Verbose "Lenovo device is detected. Continuing."
@@ -87,9 +76,8 @@ function Get-LenovoComputerModel() {
         if ($modelRegEx.Success -eq $true) {
             $global:lenovoModelNumber = $modelRegEx.Value
             Write-Verbose -Verbose "Lenovo modelnumber: $global:lenovoModelNumber - Lenovo model: $global:lenovoModel"
-	    Write-CMTraceLog -Message "Lenovo device is detected. Continuing." -Type 1
         } else {
-	    Write-Verbose -Verbose "Failed to retrieve computermodel"
+			Write-Verbose -Verbose "Failed to retrieve computermodel"
             Write-CMTraceLog -Message "Failed to retrieve computermodel" -Type 1
 			
         } 
@@ -102,17 +90,18 @@ function Get-LenovoComputerModel() {
 
 
 function Load-LSUClientModule() {
+	Write-CMTraceLog -Message "Running function: Load-LSUClientModule" -Type 1
     if (-NOT(Get-Module -Name LSUClient)) {
         Write-Verbose -Verbose "LSUClient module not loaded. Continuing."
 	Write-CMTraceLog -Message "LSUClient module not loaded. Continuing." -Type 1
         if (Get-Module -Name LSUClient -ListAvailable) {
             Write-Verbose -Verbose "LSUClient module found available. Try importing and loading it."
-	    Write-CMTraceLog -Message "LSUClient module found available. Try importing and loading it." -Type 1
+			Write-CMTraceLog -Message "LSUClient module found available. Try importing and loading it." -Type 1
             try {
 		Install-Module -Name LSUClient -Force
                 Import-Module -Name LSUClient -Force
                 Write-Verbose -Verbose "Successfully installed, imported and loaded the LSUClient module."
-		Write-CMTraceLog -Message "Successfully installed, imported and loaded the LSUClient module." -Type 1
+				Write-CMTraceLog -Message "Successfully installed, imported and loaded the LSUClient module." -Type 1
             } catch {
                 Write-Verbose -Verbose "Failed to install or import the LSUClient module. Aborting."
 		Write-CMTraceLog -Message "Failed to install or import the LSUClient module. Aborting." -Type 1
@@ -121,34 +110,57 @@ function Load-LSUClientModule() {
         }
     } else {
         Write-Verbose -Verbose "LSUClient module already installed and/or imported and loaded."
-	Write-CMTraceLog -Message "LSUClient module already installed and/or imported and loaded." -Type 1
+		Write-CMTraceLog -Message "LSUClient module already installed and/or imported and loaded." -Type 1
     }
 }
 
 
 #Function for locating and installing all drivers and BIOS which can be installed silent and unattended
 function Run-LSUClientModuleDefault() {
+    Write-CMTraceLog -Message "Running function: Run-LSUClientModuleDefault" -Type 1
     $regKey = $global:regKey
     if (-NOT(Test-Path -Path $regKey)) { New-Item -Path $regKey -Force | Out-Null }
-    #$updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended }
+    Write-Verbose -Verbose "Searching for Lenovo updates..."
+    Write-CMTraceLog -Message "Searching for Lenovo updates..." -Type 1
+
     $updates = Get-LSUpdate |
      Where-Object { $_.Installer.Unattended -AND $_.Type -eq 'Driver' -AND $_.Title -notmatch "BIOS|UEFI|Graphics|Firmware" -AND $_.RebootType -ne 5 }
-    foreach ($update in $updates) {
-        Install-LSUpdate $update -Verbose
-        New-ItemProperty -Path $regKey -Name $update.ID -Value $update.Title -Force | Out-Null
-    }
+	if ($updates -ne $null) {
+		Write-Verbose -Verbose "Downloading updates"
+		Write-CMTraceLog -Message "Downloading updates" -Type 1
+		foreach ($update in $updates) {
+			Install-LSUpdate $update -Verbose
+			New-ItemProperty -Path $regKey -Name $update.ID -Value $update.Title -Force | Out-Null
+			Write-CMTraceLog -Message "Update ID: ${update.ID}     Update Title: ${update.Title}" -Type 1
+		} 
+	} else {
+		Write-Verbose -Verbose "No updates found and/or needed."
+		Write-CMTraceLog -Message "No updates found and/or needed." -Type 1
+    }	
 }
 
 
 #Exclude Intel Graphics Driver
 #Some weird shit going on with the package here on certain models, making the script run forever, thus exlcuding the driver
 function Run-LSUClientModuleCustom() {
+    Write-CMTraceLog -Message "Running function: Run-LSUClientModuleCustom" -Type 1
     $regKey = $global:regKey
     if (-NOT(Test-Path -Path $regKey)) { New-Item -Path $regKey -Force | Out-Null }
+    Write-Verbose -Verbose "Searching for Lenovo updates..."
+	Write-CMTraceLog -Message "Searching for Lenovo updates..." -Type 1
+
     $updates = Get-LSUpdate | Where-Object { $_.Installer.Unattended -AND $_.Title -notlike "Intel HD Graphics Driver*"}
-    foreach ($update in $updates) {
-        Install-LSUpdate $update -Verbose
-        New-ItemProperty -Path $regKey -Name $update.ID -Value $update.Title -Force | Out-Null
+    if ($updates -ne $null) {
+    	Write-Verbose -Verbose "Downloading updates"
+	Write-CMTraceLog -Message "Downloading updates" -Type 1
+        foreach ($update in $updates) {
+            Install-LSUpdate $update -Verbose
+            New-ItemProperty -Path $regKey -Name $update.ID -Value $update.Title -Force | Out-Null
+            Write-CMTraceLog -Message "Update ID: ${update.ID}     Update Title: ${update.Title}" -Type 1
+		} 
+	} else {
+		Write-Verbose -Verbose "No updates found and/or needed."
+		Write-CMTraceLog -Message "No updates found and/or needed." -Type 1
     }
 }
 
@@ -159,9 +171,9 @@ $LogFolder = "C:\Windows\Logs\Software"
     
 if (!(Test-Path -path $LogFolder)){$Null = new-item -Path $LogFolder -ItemType Directory -Force}
 
-$ScriptVer = "2022.05.02"
+$ScriptVer = "2022.05.13"
 $Component = "CloudOSD_LSUClient"
-$LogFile = "$LogFolder\CloudOSD_LSUClient.log"
+$LogFile = "$LogFolder\CloudOSD_LSUClient_$(Get-Date -format yyyy-MM-dd-HHmm).log"
 
 Write-Output "Starting script to Install Lenovo Drivers and BIOS"
 
@@ -171,17 +183,37 @@ Write-CMTraceLog -Message "=====================================================
 Write-CMTraceLog -Message "Running Script as $env:USERNAME" -Type 1 
 
 
+## Set the Installation Policy for PSRepository for PSGallery
+Try { Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    } Catch {
+    write-output "Failed to set PSReposity Installation Policy to Trusted for PSGallery"
+    Write-CMTraceLog -Message "Failed to set PSReposity Installation Policy to Trusted for PSGallery" -Type 3
+    }
+
+
+## Install the module LSUClient
+Try { Install-Module -Name LSUClient -Force
+    } Catch {
+    write-output "Failed to install the LSUClient PowerShell Module"
+    Write-CMTraceLog -Message "Failed to install the LSUClient PowerShell Module" -Type 3
+    }
+	
+## Import the module LSUClient
+Try { Import-Module -Name LSUClient -Force
+    } Catch {
+    write-output "Failed to load the LSUClient PowerShell Module"
+    Write-CMTraceLog -Message "Failed to load the LSUClient PowerShell Module" -Type 3
+    }
+
 
 
 try {
-	Write-CMTraceLog -Message "Starting script to Install Lenovo Drivers and BIOS" -Type 1
+    Write-CMTraceLog -Message "Starting script to Install Lenovo Drivers and BIOS" -Type 1
     Get-LenovoComputerModel
     Load-LSUClientModule
     if ($global:lenovoModelNumber -eq "20QF") {
-        Write-CMTraceLog -Message "Running LSUClient with custom function" -Type 1 
         Run-LSUClientModuleCustom
     } else {
-        Write-CMTraceLog -Message "Running LSUClient with default function" -Type 1 
         Run-LSUClientModuleDefault
     }
 }
