95f90ae3ffd0fd48cfa67d22b481c2708f3574f8
Jeremy Thurgood <80785999+jwthurgood@users.noreply.github.com>
2022-05-19 16:23:55 -0700

    Update Install-Choco.ps1

--- OSD/CloudOSD/Install-Choco.ps1
+++ OSD/CloudOSD/Install-Choco.ps1
@@ -10,6 +10,7 @@ get-packageprovider -Name Chocolatey -Force
 
 # Set as trusted
 Set-PackageSource -Name Chocolatey -Trusted -Force
+Set-PackageSource -Name PSGallery -Trusted -Force
 
 # Run the following commands to enable TLS 1.2 support.  Enables Nuget and Chocolatey to return results when using find-package (as Nuget and Chocolatey require TLS 1.2 or higher)
 # The SystemDefaultTlsVersions setting allows .NET to use the OS configuration (which in Windows 11 is TLS 1.3 & 1.2)
