71e9cc64f0cd5ee211b31f80e2eceaf9a247ad8a
Jeremy Thurgood <80785999+jwthurgood@users.noreply.github.com>
2022-06-08 10:16:38 -0700

    Update set-tsdate-variable.ps1

--- OSD/CloudOSD/set-tsdate-variable.ps1
+++ OSD/CloudOSD/set-tsdate-variable.ps1
@@ -1,6 +1,14 @@
-
+<#
+    Name: Set-TSdate-Variable.ps1
+    Version: 1.0
+    Author: Jeremy Thurgood, DWT
+    Date: 2022-06-08
+    Command: powershell.exe -executionpolicy bypass -file Set-TSdate-Variable.ps1
+    Usage:  Run in MEMCM Task Sequence to set Date/time variable.
+            
+#>
 
 
 $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
 
-$tsenv.Value("TSDate") = Get-Date -format "yyyyMMdd-hhmm"
+$tsenv.Value("TSDate") = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
